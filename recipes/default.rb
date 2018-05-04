252ea2ff0f304e01a951e2d2393157110c496c5e
Muhammad Sultan <mohammadsultan@yahoo.com>
2018-05-04 11:28:23 -0400

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,14 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<html>
+   <h1>Hello World!</h1>
+</html>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
